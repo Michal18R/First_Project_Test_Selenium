ae70a47c3d1718305d697ec21350d095eda73c1e
Hedi18 <studiawencyl@gmail.com>
2023-04-30 20:44:31 +0200

    solving the problem when a city other than Dubai was entered in the city field - for example, London

--- src/test/java/pl/seleniumdemo/pages/HotelSearchPage.java
+++ src/test/java/pl/seleniumdemo/pages/HotelSearchPage.java
@@ -6,18 +6,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class HotelSearchPage {
     @FindBy(xpath = "//span[text()='Search by Hotel or City Name']")
     private  WebElement searchHotelSpan;
 
     @FindBy(xpath = "//div[@id='select2-drop']//input")
     private WebElement searchHotelInput;
-
-    @FindBy(xpath = "//span[@class='select2-match' and text()='Dubai']")
-    private WebElement hotelMatch;
 
     @FindBy(name = "checkin")
     private WebElement checkInImput;
@@ -35,16 +29,21 @@ public class HotelSearchPage {
     private WebElement childPlusBtn;
 
     @FindBy(xpath = "//button[text()=' Search']")
-    public WebElement searchButton;
+    private WebElement searchButton;
 
+    private WebDriver driver;
     public HotelSearchPage(WebDriver driver){
+
         PageFactory.initElements(driver,this);
+        this.driver = driver;
     }
 
     public void setCity(String cityName){
         searchHotelSpan.click();
         searchHotelInput.sendKeys(cityName);
-        hotelMatch.click();
+        String xpath = String.format("//span[@class='select2-match' and text()='%s']", cityName);
+        driver.findElement(By.xpath(xpath)).click();
+
     }
 
     public void setDates(String checkIn, String checkOut){

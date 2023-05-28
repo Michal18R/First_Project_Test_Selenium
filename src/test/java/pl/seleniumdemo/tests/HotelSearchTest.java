c1e15b918025730ab94cd59b5122542c8713c1b5
Hedi18 <studiawencyl@gmail.com>
2023-05-28 21:18:52 +0200

    add Test with Data Provider

--- src/test/java/pl/seleniumdemo/tests/HotelSearchTest.java
+++ src/test/java/pl/seleniumdemo/tests/HotelSearchTest.java
@@ -2,10 +2,13 @@ package pl.seleniumdemo.tests;
 
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pl.seleniumdemo.pages.HotelSearchPage;
 import pl.seleniumdemo.pages.ResultsPage;
+import pl.seleniumdemo.utils.ExcelReader;
 
+import java.io.IOException;
 import java.util.List;
 
 
@@ -59,5 +62,34 @@ public class HotelSearchTest extends BaseTest {
         Assert.assertEquals("Hyatt Regency Perth", hotelNames.get(3));
 
 
+    }
+
+    @Test(dataProvider = "data")
+    public void searchHotelTestWithDataProvider(String city, String hotel) throws InterruptedException {
+
+
+        HotelSearchPage hotelSearchPage = new HotelSearchPage(driver);
+        hotelSearchPage.setCity(city);
+        hotelSearchPage.setDates("27/04/2021", "29/04/2021");
+        hotelSearchPage.setTravelers(2, 2);
+        hotelSearchPage.performSearch();
+
+
+        ResultsPage resultsPage = new ResultsPage(driver);
+
+        //pobieranie nazw hoteli do listy
+        List<String> hotelNames = resultsPage.getHotelNames();
+
+        hotelNames.forEach(System.out::println);
+
+        //Assercje
+        Assert.assertEquals(hotelNames.get(0) , hotel);
+
+
+    }
+
+    @DataProvider
+    public Object[][] data() throws IOException {
+        return ExcelReader.readExcel("testData.xls");
     }
 }
